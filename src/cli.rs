3336a776e595d54b39cb8e99f191dace6cd6c3d9
Milan Suk <Milansuk@email.cz>
2021-10-31 21:39:52 +0100

    :books: add about for all arguments (#5)

--- src/cli.rs
+++ src/cli.rs
@@ -5,25 +5,48 @@ use crate::{commit_message::CommitMessageInput, emoji::COMMIT_TYPES};
 #[derive(Parser, Debug)]
 #[clap(version = "0.1", author = "Milan Suk <Milansuk@email.cz>")]
 pub struct Opts {
-    #[clap(short, long)]
+    #[clap(short, long, about = "Commit message")]
     pub message: String,
 
     #[clap(short='t', long, possible_values=COMMIT_TYPES)]
     pub commit_type: String,
 
-    #[clap(short, long)]
+    #[clap(
+        short,
+        long,
+        about = "Optional information specifying the section of the codebase"
+    )]
     pub scope: Option<String>,
 
-    #[clap(short, long)]
+    #[clap(
+        short,
+        long,
+        about = "Identifier of the github issue / jira issue / ..."
+    )]
     pub issue: Option<String>,
 
-    #[clap(short, long, takes_value = false)]
+    #[clap(
+        short,
+        long,
+        takes_value = false,
+        about = "If turned on, an emoji will be used instead of name of the commit type"
+    )]
     pub emoji: bool,
 
-    #[clap(short, long, takes_value = false)]
+    #[clap(
+        short,
+        long,
+        takes_value = false,
+        about = "-a option for the git commit command"
+    )]
     pub add_all_files: bool,
 
-    #[clap(short, long, takes_value = false)]
+    #[clap(
+        short,
+        long,
+        takes_value = false,
+        about = "Don't do the commit, only show the commit message"
+    )]
     pub dry_run: bool,
 }
 
